485ab87acce21d09a8123de3b8b620e7a34fd205
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2024-03-21 04:30:31 +0800

    feat: Add models and slide AR view

--- page/ar/index.js
+++ page/ar/index.js
@@ -1,1 +1,21 @@
-lightGallery(document.getElementById('lightgallery'));
+let lg = document.getElementById('lightgallery');
+
+lg.addEventListener(
+    "onSlideClick",
+    function(event){
+        // event.target is slide parent
+        // let href = event.target?.getAttribute("ext-slide-href");
+        let lg_uid = event.target?.getAttribute("lg-uid");
+        let index = window.lgData[lg_uid].index;
+        let href = event.target?.children[index].getAttribute('ext-slide-href');
+
+        if (href !== null) {
+            window.open(href);
+        } else {
+            console.error(`Failed to load slide href of target: ${event.target}`);
+            console.error(event);
+        }
+    }
+);
+
+lightGallery(lg);
